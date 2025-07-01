336e53f8fa0d6b21af757a670fbda78b2c48401e
hwq <hwq@juexiaotime.com>
2025-07-01 18:13:21 +0800

    fix: base path

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import * as path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: './',
+  base: './xiaomi-su7',
   resolve: {
     // @ts-ignore
     alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }]
