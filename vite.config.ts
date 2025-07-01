04d21a634cf09eccb07acada5a7eef5b4d949c1b
hwq <hwq@juexiaotime.com>
2025-07-01 17:57:30 +0800

    feat: public path

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import AutoImport from 'unplugin-auto-import/vite'
+// @ts-ignore
 import * as path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: './',
   resolve: {
     // @ts-ignore
     alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }]
